71cdb8ed8526ee563a5ad667045563c8e33ef223
kashifnajeeb <aashinajeeb@gmail.com>
2024-05-03 14:18:12 +0200

    Task 9 completed

--- script.js
+++ script.js
@@ -113,11 +113,13 @@ function getTotalCellsToClear() {
 }
 
 function checkForVictory() {
-  //
-  // TODO: Task 10 - Implement victory. If the player has revealed as many cells as they must (every cell that isn't a
-  //                 bomb), set variable victory to true.
-  //
-  return 0;
+  const totalCellsToClear = getTotalCellsToClear();
+
+  const clearedCells = getClearedCells();
+
+  if (clearedCells === totalCellsToClear) {
+    victory = true;
+  }
 }
 
 //
--- test.js
+++ test.js
@@ -1,30 +1,7 @@
-function getBombsCount() {
-  // Calculate the number of bombs by iterating over all cells and counting the ones that are bombs
-  let bombCount = 0;
-  for (let row = 0; row < ROWS_COUNT; row++) {
-    for (let col = 0; col < COLS_COUNT; col++) {
-      if (cells[row][col].isBomb) {
-        bombCount++;
-      }
-    }
-  }
-  return bombCount;
-}
-
-function getClearedCells() {
-  // Calculate the number of cleared cells by counting the ones that have been discovered
-  let clearedCount = 0;
-  for (let row = 0; row < ROWS_COUNT; row++) {
-    for (let col = 0; col < COLS_COUNT; col++) {
-      if (cells[row][col].discovered) {
-        clearedCount++;
-      }
-    }
-  }
-  return clearedCount;
-}
-
-function getTotalCellsToClear() {
-  // Calculate the total number of cells to clear, which is the total number of cells minus the number of bombs
-  return ROWS_COUNT * COLS_COUNT - getBombsCount();
+function checkForVictory() {
+  //
+  // TODO: Task 10 - Implement victory. If the player has revealed as many cells as they must (every cell that isn't a
+  //                 bomb), set variable victory to true.
+  //
+  return 0;
 }
